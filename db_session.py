5cc038c377e2b6d00a76f7b305b1eaeb9acb6fb0
Joma-kun <jomamiwa@gmail.com>
2024-05-18 19:11:17 +0900

    make POST & GET func in db_session

---
+++ db_session.py
@@ -0,0 +1,58 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+import os
+import requests
+
+URL="https://cnkgbbz9x1h5.cybozu.com/k/v1/"
+APPID=1
+API_TOKEN=os.getenv("API_TOKEN")
+
+PARAMS={
+  "app":APPID,
+  "record":{
+    "worker":{
+      "value":"鈴木"
+    },
+    "task":{
+        "value":"すやすや寝る"
+    },
+    "time":{
+        "value":"100"
+    }
+  }
+}
+
+def post_kintone(url,api_token,params):
+    headers={"X-Cybozu-API-Token":api_token,"Content-Type":"application/json"}
+    resp=requests.post(url+"record.json",json=params,headers=headers)
+
+    return resp
+
+def get_kintone(url, api_token, app_id):
+    headers={"X-Cybozu-API-Token":api_token}
+    params = {"app": app_id}
+    resp = requests.get(url + "records.json", headers=headers, params=params)
+
+    return resp
+
+if __name__=="__main__":
+    # POST
+    # resp=post_kintone(URL,API_TOKEN,PARAMS)
+    # print(resp.text)
+
+    # GET
+    resp = get_kintone(URL, API_TOKEN, APPID)
+    if resp.status_code == 200:
+        data = resp.json()
+        records = data.get("records", [])
+        for record in records:
+            task = record.get("task", {}).get("value", "")
+            time = record.get("time", {}).get("value", "")
+            worker = record.get("worker", {}).get("value", "")
+            print("Task:", task)
+            print("Time:", time)
+            print("Worker:", worker)
+            print("--------------------")
+    else:
+        print("Error:", resp.status_code)
