9c1362db46c4f9343539b32efafadd929b45cc2c
imayamu <imamonsp0216@icloud.com>
2024-05-19 00:06:53 +0900

    qa

---
+++ client_session_qa.py
@@ -0,0 +1,75 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from db_session import post_todo, get_todo
+import json
+
+app = FastAPI()
+
+class Todo(BaseModel):
+    worker: str
+    task: str
+    time: int
+
+class qa_archive(BaseModel):
+    questioner: str
+    respondent: str
+    question: str
+    answer: str
+    evaluation: int
+    comment: str
+
+APPID=1
+
+@app.post("/qa/post")
+async def qa_register(qa: qa_archive, status_code=201):
+    # todo_data = todo.model_dump()
+
+    qa_data = {
+        "app": APPID,
+        "record":{
+            "questioner":{
+                "value": qa.questioner
+            },
+            "respondent":{
+                "value": qa.respondent
+            },
+            "question":{
+                "value": qa.question
+            },
+            "answer":{
+                "value": qa.answer
+            },
+            "evaluation":{
+                "value": qa.evaluation
+            },
+            "comment":{
+                "value": qa.comment
+            }
+            
+        }
+    }
+
+    try:
+        post_qa(qa_data)
+        return {"message": "Qa registered successfully"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Failed to register Qa: {str(e)}")
+
+
+@app.get("/qa/get")
+async def qa_get():
+    try:
+        return get_qa()
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Failed to get Qa: {str(e)}")
+
+# @app.post("/qa_archive/post")
+# async def qa_archive_register(qa_archive: qa_archive, status_code=201):
+
+#     qa_archive_data = qa_archive.model_dump()
+
+#     try:
+#         await (qa_archive_data)
+#         return {"message": "QA Archive registered successfully"}
+#     except Exception as e:
+#         raise HTTPException(status_code=500, detail=f"Failed to register QA Archive: {str(e)}")
---
+++ db_session_qa.py
@@ -0,0 +1,82 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+import os
+import requests
+
+URL="https://cnkgbbz9x1h5.cybozu.com/k/v1/"
+APPID=1
+API_TOKEN=os.getenv("API_TOKEN")
+
+PARAMS={
+  "app":APPID,
+  "record":{
+    "worker":{
+      "value":"鈴木"
+    },
+    "task":{
+        "value":"すやすや寝る"
+    },
+    "time":{
+        "value":"100"
+    }
+  }
+}
+PARAMS={
+    "app": APPID,
+    "record":{
+        "questioner":{
+            "value":質問者 
+        },
+        "respondent":{
+            "value": 回答者
+        },
+        "question":{
+        "value": 質問
+        },
+        "answer":{
+            "value": 答え
+        },
+        "evaluation":{
+            "value": 1
+        },
+        "comment":{
+            "value": 良い質問
+        }
+            
+    }
+}
+
+def post_qa(params):
+    headers={"X-Cybozu-API-Token":API_TOKEN,"Content-Type":"application/json"}
+    resp=requests.post(URL+"record.json",json=params,headers=headers)
+
+    return resp
+
+def get_qa():
+    headers={"X-Cybozu-API-Token":API_TOKEN}
+    params = {"app": APPID}
+    resp = requests.get(URL + "records.json", headers=headers, params=params)
+
+    return resp.json()
+
+# if __name__=="__main__":
+#     # POST
+#     resp=post_todo(PARAMS)
+#     print(resp.text)
+
+    # GET
+    # resp = get_todo()
+    # if resp.status_code == 200:
+    #     data = resp.json()
+    #     records = data.get("records", [])
+    #     for record in records:
+    #         task = record.get("task", {}).get("value", "")
+    #         time = record.get("time", {}).get("value", "")
+    #         worker = record.get("worker", {}).get("value", "")
+    #         print("Task:", task)
+    #         print("Time:", time)
+    #         print("Worker:", worker)
+    #         print("--------------------")
+    # else:
+    #     print("Error:", resp.status_code)
